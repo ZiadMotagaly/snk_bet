40f28a854b2c3de060eae883516e4bc7cda6a078
ZiadMotagaly <ziad.motagaly@gmail.com>
2024-03-19 08:07:43 +0100

    Update data_crawler.py

--- data_crawler.py
+++ data_crawler.py
@@ -96,6 +96,9 @@ def player_advancement():
 
 def player_recent_matches():
     url = "https://www.snooker.org/res/index.asp?player=12"
+    #https://www.snooker.org/res/index.asp?player=12
+    #https://www.snooker.org/res/index.asp?season=-1&player=12
+    
 
     # Send a GET request to the webpage
     response = requests.get(url)
@@ -107,9 +110,35 @@ def player_recent_matches():
         
         # Find all <tr> elements with class name starting with 'gradeA'
         tr_elements = soup.find_all("tr", class_=lambda x: x and x.startswith('gradeA even'))
-        print(tr_elements)
+        #print(tr_elements)
 
-  
+        thead_elements = soup.find_all('thead')
+        #print(thead_element)
+        for thead_element in thead_elements:
+            # Find all <tr> elements under the current <thead> element
+            in_tr_elements = thead_element.find_all('tr')
+            for in_tr_element in in_tr_elements:
+                    # Find all <a> elements under the current <tr> element with an href attribute starting with '/res'
+                    a_elements = in_tr_element.find_all('a', href=lambda href: href and href.startswith('/res'))
+                
+                # Extract the text from each <a> element
+                    for a_element in a_elements:
+                        headers = a_element.get_text(strip=True)
+                        print(headers)
+            
+
+
+        # Find the <a> element within the <thead> element
+        # Find all <a> elements
+        #a_elements = soup.find_all('a')
+        a_elements = soup.find_all('a', href=lambda i: i and i.startswith('/res'))
+
+
+        # Extract the text from each <a> element
+        for a_element in a_elements: 
+            text = a_element.get_text(strip=True)
+            #print(text)
+        
         
         # Extract titles from the <td> elements within the selected <tr> elements
         for tr in tr_elements:
@@ -119,8 +148,7 @@ def player_recent_matches():
             
             titles = [td.text.strip() for td in td_elements if td.get('class') and 'round' in td.get('class')]
             if titles:
-                pass
-               # print(titles[0])
+                print(titles[0])
             
             '''first_score = [td.text.strip() for td in td_elements if td.get('class') and 'score  first-score' in td.get('class')]
             if first_score:
@@ -134,12 +162,12 @@ def player_recent_matches():
             first_score = td_elements[3].text.strip()
             last_score = td_elements[5].text.strip()
 
-            #print(first_score,last_score)
+            print(first_score,last_score)
 
             player_names = [td.text.strip() for td in td_elements if td.get('class') and 'player' in td.get('class')]
             if player_names:
                 pass
-                #print(player_names[0], ' ', player_names[1])
+                print(player_names[0], ' ', player_names[1])
 
                
             
@@ -147,9 +175,64 @@ def player_recent_matches():
     else:
         print("Failed to retrieve webpage.")
 
+def get_table_elements():
+
+    url = "https://www.snooker.org/res/index.asp?player=12"
+    #https://www.snooker.org/res/index.asp?player=12
+    #https://www.snooker.org/res/index.asp?season=-1&player=12
+    
+
+    # Send a GET request to the webpage
+    response = requests.get(url)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        # Parse the HTML content of the webpage
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Find the table element
+        table = soup.find('table', class_='display matches')
+
+        '''# Iterate through the children of the table
+        for child in table.children:
+
+            # Check if the child is a thead or tbody element
+            if child.name == 'thead' or child.name == 'tbody':
+
+                # Find all the text within the child element
+                text = child.get_text(strip=True)
+                print(text)
+        '''
+
+        if table:
+            # Find all thead and tbody elements within the table
+            thead_elements = table.find_all('thead')
+            tbody_elements = table.find_all('tbody')
+            
+            # Iterate over the pairs of thead and tbody elements
+            for thead, tbody in zip(thead_elements, tbody_elements):
+                # Extract text from the thead element
+                thead_text = thead.get_text(strip=True)
+                print(thead_text)
+                
+                #Extract text from the tbody element
+                tr_elements = tbody.find_all("tr", class_=lambda c: c and c.startswith('gradeA'))
+        
+                # Iterate over the <tr> elements
+                for tr in tr_elements:
+                    # Find all <td> elements within the <tr> element
+                    td_elements = tr.find_all("td")
+                    
+                    # Extract text from specific <td> elements
+                    titles = [td.text.strip() for td in td_elements if td.get('class') and 'round' in td.get('class')]
+                    print(titles[0])
+        else:
+            print("Table with class 'display matches' not found.")
+
+            
 if __name__ == "__main__":
     #crawl_cuetracker()
     #table()
     #player_advancement()
-    player_recent_matches()
-
+    #player_recent_matches()
+    get_table_elements()
